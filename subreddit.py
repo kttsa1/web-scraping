f6b9d9afb94e5e9455bb7d08bab00ca4dfc9a21b
Katie Tsai <tsaikatie3@gmail.com>
2022-08-06 15:36:13 -0400

    scraping for hot posts

--- subreddit.py
+++ subreddit.py
@@ -1,2 +1,5 @@
 import praw
 reddit = praw.Reddit(client_id='-v1GeBEDL0aPvVu6wahwfA', client_secret='LNUeU3rPUlho8HA_V7QOu-RuPQSFMA', user_agent='WebScraping')
+hot_posts = reddit.subreddit('MachineLearning').hot(limit=10)
+for post in hot_posts:
+    print(post.title)
